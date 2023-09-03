3f9b9c8f1d58902cebb78d11ed0b1b64f1f1eab0
Cecilia Maza <122183118+CeciMaza@users.noreply.github.com>
2023-09-03 14:58:57 -0300

    Modif Ago

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolioCM/portfolioCM/PortfolioCmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioCM.portfolioCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioCmApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador/BackEnd/src/test/java/com/portfolioCM/portfolioCM/PortfolioCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioCM.portfolioCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
